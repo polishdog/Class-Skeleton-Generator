7db2a31ea7a53a0c84d8d3fa9a3fbcc66fef2308
agnieszkapiszcz <agnieszkapiszcz@192.168.1.13>
2015-01-14 22:12:07 -0800

    Test Commit2

--- Class Skeleton Generator/src/pkgclass/skeleton/generator/ClassSkeletonGenerator.java
+++ Class Skeleton Generator/src/pkgclass/skeleton/generator/ClassSkeletonGenerator.java
@@ -18,6 +18,7 @@ public class ClassSkeletonGenerator
     public static void main(String[] args)
     {
         System.out.println("HelloWorld");
+        System.out.println("Hello World!");
     }
     
 }
